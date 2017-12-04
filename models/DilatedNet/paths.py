12bd9bfd80acf44e989b514183b2ff3f292e28e2
Amado Antonini <amadoa@mit.edu>
2017-12-04 16:04:06 -0500

    download data to ucruncher

--- models/DilatedNet/paths.py
+++ models/DilatedNet/paths.py
@@ -1,1 +1,5 @@
-data_path = '/home/amado/Dropbox (MIT)/Local/Fall17/Computer Vision/Scene_Segmentation/data/'
+# amado ubuntu
+#data_path = '/home/amado/Dropbox (MIT)/Local/Fall17/Computer Vision/Scene_Segmentation/data/'
+
+# cruncher
+data_path = '~/picpart/data/'
