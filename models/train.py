e7441d2fac6059c88ea6531d8a93d9d88b150207
Amado Antonini <amadoa@mit.edu>
2017-12-06 13:53:57 -0500

    load snapshots from training script

--- models/train.py
+++ models/train.py
@@ -1,6 +1,8 @@
 import caffe
 import yaml
 
+#restore_from = '../snapshots/DilatedNet_Dec_05/snapshot_iter_190000'
+restore_from = ''
 
 with open('../util/config.yaml', 'r') as f:
     config = yaml.load(f)
@@ -11,8 +13,12 @@ if GPU:
     caffe.set_mode_gpu()
     caffe.set_device(0)
 
-
-solver = caffe.get_solver('DilatedNet/solver_DilatedNet.prototxt')
+solver = None
+if len(restore_from) == 0:
+    solver = caffe.get_solver('DilatedNet/solver_DilatedNet.prototxt')
+else:
+    solver = caffe.get_solver(restore_from+'.caffemodel')
+    solver.restore(restore_from+'.solverstate')
 #solver = caffe.get_solver('FCN/solver_FCN.prototxt')
 
 
