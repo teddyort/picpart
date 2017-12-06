db7fafab9f3dac3e48f0a796d46bcd66719c7e61
Amado Antonini <amadoa@mit.edu>
2017-12-06 00:27:35 -0500

    finished training dilated net and saved copies of training files

---
+++ snapshots/DilatedNet_Dec_05/train.py
@@ -0,0 +1,19 @@
+import caffe
+import yaml
+
+
+with open('../util/config.yaml', 'r') as f:
+    config = yaml.load(f)
+    
+GPU = config['GPU']
+
+if GPU:
+    caffe.set_mode_gpu()
+    caffe.set_device(0)
+
+
+solver = caffe.get_solver('DilatedNet/solver_DilatedNet.prototxt')
+#solver = caffe.get_solver('FCN/solver_FCN.prototxt')
+
+
+solver.solve()
