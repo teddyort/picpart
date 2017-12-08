4a734e257ab573b21ac715afa49e32f3b4f8801e
Amado Antonini <amadoa@mit.edu>
2017-12-08 14:04:03 -0500

    compare IoU function against one provided

--- models/IoU_layer.py
+++ models/IoU_layer.py
@@ -6,6 +6,10 @@ Created on Wed Dec  6 11:06:08 2017
 """
 import caffe
 import numpy as np
+import sys
+sys.path.append('../util/')
+from utils_eval import intersectionAndUnion
+
 np.seterr(divide='ignore', invalid='ignore')
 
 class IoULayer(caffe.Layer):
@@ -44,18 +48,30 @@ class IoULayer(caffe.Layer):
         # predictions must go first in the prototxt definition
         predictions = bottom[0].data
         labels = bottom[1].data
-        for i in range(labels.shape[0]):
+        batch_size = labels.shape[0]
+        for i in range(batch_size):
             hist = self.fast_hist(labels[i,...].flatten(), predictions[i,...].argmax(0).flatten())
             self.hist += hist
-        IoU = np.diag(hist)[1:]/(hist.sum(1)[1:]+hist.sum(0)[1:]-np.diag(hist)[1:]+1e-10)
+        IoU = np.diag(hist)[1:]/(hist.sum(1)[1:]+hist[1:,:].sum(0)[1:]-np.diag(hist)[1:])
         meanIoU = np.mean(np.nan_to_num(IoU))
+        
+        area_intersection = np.zeros([150,batch_size])
+        area_union = np.zeros([150,batch_size])
+        for i in range(batch_size):
+            (area_intersection[:,i], area_union[:,i]) = intersectionAndUnion(predictions[i,...].argmax(0), labels[i,...],150)
+        IoU2 = 1.0 * np.sum(area_intersection, axis=1) / np.sum(np.spacing(1)+area_union, axis=1)
+        meanIoU2 = np.mean(IoU2)
+        
         top[0].data[...] = meanIoU
         if self.verbose:
             print('First 30 predictions of first image in batch:')
             print(predictions[0,...].argmax(0).flatten()[0:30])
             print('Top 10 x 4 of hist:')
             print(self.hist[0:10,0:4])
+        if self.verbose or self.phase == 1:
             print('mean IoU: {}'.format(meanIoU))
+            print('mean IoU2:{}'.format(meanIoU2))
+            
         
     
     def backward(self, top, propagate_down, bottom):
