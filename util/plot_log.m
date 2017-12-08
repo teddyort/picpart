7eb620022f52386ca45d05b1256b3fdd8911ff6c
Amado Antonini <amadoa@mit.edu>
2017-12-08 14:04:58 -0500

    dock matlab figures properly

--- util/plot_log.m
+++ util/plot_log.m
@@ -1,22 +1,30 @@
 clear, clc, clf
-set(0,'DefaultFigureWindowStyle','docked')
 addpath(genpath('yaml_matlab')); 
 config = ReadYaml('config.yaml');
 log_folder = [config.dropbox,'logs/'];
 % log_train_name = 'FCN_train_log_Dec08_0215.txt';
 % log_test_name = 'FCN_test_log_Dec08_0215.txt';
-log_train_name = 'DilatedNet_train_log_Dec08_0230.txt';
-log_test_name = 'DilatedNet_test_log_Dec08_0230.txt';
+% log_train_name = 'DilatedNet_train_log_Dec08_0230.txt';
+% log_test_name = 'DilatedNet_test_log_Dec08_0230.txt';
+log_train_name = 'DilatedNet_train_log_Dec08_1115.txt';
+log_test_name = 'DilatedNet_test_log_Dec08_1115.txt';
 log_train_file = [log_folder,log_train_name];
 T_train = readtable(log_train_file,'Delimiter',',','HeaderLines',0,'ReadVariableNames',true);
 vars = T_train.Properties.VariableNames;
 
+N = size(vars,2)-2;
+for i = 1:N
+    h(i) = figure(i);
+end
+set(h,'WindowStyle','Docked');
+
 xname = vars{1};
 xdata_train = T_train.(xname);
-k = round(length(xdata_train)/20);
-for i = 3:size(vars,2)
-    yname = vars{i};
-    figure(i-2)
+k = max(round(length(xdata_train)/20),1);
+% k = 1
+for i = 1:N
+    yname = vars{i+2};
+    figure(i)
     ydata_train = movmean(T_train.(yname),k);
     plot(xdata_train,ydata_train)
     title(yname)
@@ -31,10 +39,10 @@ if validation_too
     log_test_file = [log_folder,log_test_name];
     T_test= readtable(log_test_file,'Delimiter',',','HeaderLines',0,'ReadVariableNames',true);
     xdata_test = test_interval*T_test.(xname);
-    for i = 3:size(vars,2)
-        yname = vars{i};
-        ydata_test = movmean(T_test.(yname),round(k/test_interval));
-        figure(i-2)
+    for i = 1:N
+        yname = vars{i+2};
+        ydata_test = movmean(T_test.(yname),max(round(k/test_interval),1));
+        figure(i)
         hold on
         plot(xdata_test,ydata_test)
         hold off
@@ -42,4 +50,3 @@ if validation_too
     end
 end
 
-set(0,'DefaultFigureWindowStyle','normal')
