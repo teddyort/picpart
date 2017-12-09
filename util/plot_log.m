a2ef46bf159745a8f88433df97c589a4a044b7fb
Amado Antonini <amadoa@mit.edu>
2017-12-09 12:22:56 -0500

    flag to save figures

--- util/plot_log.m
+++ util/plot_log.m
@@ -2,12 +2,16 @@ clear, clc, clf
 addpath(genpath('yaml_matlab')); 
 config = ReadYaml('config.yaml');
 log_folder = [config.dropbox,'logs/'];
-log_train_name = 'FCN_train_log_Dec08_0215.txt';
-log_test_name = 'FCN_test_log_Dec08_0215.txt';
+% log_train_name = 'FCN_train_log_Dec08_0215.txt';
+% log_test_name = 'FCN_test_log_Dec08_0215.txt';
 % log_train_name = 'DilatedNet_train_log_Dec08_0230.txt';
 % log_test_name = 'DilatedNet_test_log_Dec08_0230.txt';
 % log_train_name = 'DilatedNet_train_log_Dec08_1115.txt';
 % log_test_name = 'DilatedNet_test_log_Dec08_1115.txt';
+% log_train_name = 'DilatedNet_train_log_Dec09_0430.txt';
+% log_test_name = 'DilatedNet_test_log_Dec09_0430.txt';
+log_train_name = 'DilatedNet_train_log_Dec09_1200.txt';
+log_test_name = 'DilatedNet_test_log_Dec09_1200.txt';
 log_train_file = [log_folder,log_train_name];
 T_train = readtable(log_train_file,'Delimiter',',','HeaderLines',0,...
     'ReadVariableNames',true);
@@ -21,7 +25,7 @@ set(h,'WindowStyle','Docked');
 
 xname = vars{1};
 xdata_train = T_train.(xname);
-k = max(round(length(xdata_train)/20),1);
+k = max(round(length(xdata_train)/10),1);
 % k = 1
 for i = 1:N
     yname = vars{i+2};
@@ -42,7 +46,7 @@ if validation_too
     xdata_test = test_interval*T_test.(xname);
     for i = 1:N
         yname = vars{i+2};
-        ydata_test = movmean(T_test.(yname),max(round(k/test_interval),1));
+        ydata_test = movmean(T_test.(yname),max(round(4*k/test_interval),1));
         figure(i)
         hold on
         plot(xdata_test,ydata_test)
@@ -52,7 +56,11 @@ if validation_too
 end
 
 %% save figures as png's for quick reference
-for i = 1:N
-    saveas(figure(i),[log_train_file(1:end-4),'_',vars{i+2},'.png'])
-    
+print_figures = false;
+if print_figures
+    for i = 1:N
+        figure_file = [log_train_file(1:end-4),'_',vars{i+2},'.png'];
+        saveas(figure(i),figure_file)
+        fprintf('saved %s\n',figure_file)
+    end
 end
